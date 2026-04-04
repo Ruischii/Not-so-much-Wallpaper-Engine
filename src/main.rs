e6739872fe958bcbdd9604d137190cffba6216fa
Rui <gintatsu3@gmail.com>
2026-04-04 15:14:51 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod engine;
 mod wayland;
+mod wallpaper;
 
 use engine::Engine;
 
 fn main() {
     let engine = Engine::new();
+      wallpaper::run().unwrap();
      wayland::run_wayland_wallpaper()
         .expect("wayland failed");
     engine.run();
