01a2a120480aabe565e42c9684bcbc12012f1821
Rui <gintatsu3@gmail.com>
2026-04-04 14:43:36 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ use engine::Engine;
 
 fn main() {
     let engine = Engine::new();
+    let app = WallpaperGui;
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
     engine.run();
 }
